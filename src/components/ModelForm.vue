894ac2eeaf0da8095d810634df78fc313ad817b6
Tiago Higuita <tiagohig70@gmail.com>
2023-12-06 18:50:46 -0500

    Form part

---
+++ src/components/ModelForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <form>
+        <input type="text" v-model="inputText">
+    </form>
+    <h3>{{ inputText }}</h3>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    data() {
+        return { inputText: '' }
+    }
+});
+</script>
+
+<style scoped></style>
