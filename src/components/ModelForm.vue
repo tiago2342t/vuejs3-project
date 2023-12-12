42f84719b639dee604ece5201c5547c634e59ab4
Tiago Higuita <tiagohig70@gmail.com>
2023-12-12 17:58:05 -0500

    ModelForm update

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -1,17 +1,22 @@
 <template>
     <form>
         <input type="text" v-model="inputText">
+        <input type="checkbox" v-model="agree"> Acepto los términos y condiciones
+        <select v-model="country">
+            <option value="Spain">Spain</option>
+            <option value="UK">UK</option>
+            <option value="France">France</option>
+        </select>
     </form>
     <h3>{{ inputText }}</h3>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-    data() {
-        return { inputText: '' }
-    }
-});
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+let inputText = ref('')
+let agree = ref(true)
+let country = ref('Spain')
 </script>
 
 <style scoped></style>
