0a8424added9062e55867a503827c9c0689c7c9c
tiago <tiagohig70@gmail.com>
2023-12-18 20:59:56 -0500

    Custom directives

--- src/components/ComputedExample.vue
+++ src/components/ComputedExample.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <label>Subtotal: </label>
+        <label v-custom-font.red.xxl>Subtotal: </label>
         <input type="number" v-model="subtotal">
-        <h3>Total impuestos a pagar: {{ totalVat }} ({{ vat }}%)</h3>
+        <h3 v-custom-font.xxl.blue>Total impuestos a pagar: {{ totalVat }} ({{ vat }}%)</h3>
     </div>
 </template>
 
